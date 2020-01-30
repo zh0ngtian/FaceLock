eddf2220af687ba06132212fd282c1d623631612
YaoZhontian <305301890@qq.com>
2020-01-30 13:51:46 +0800

    Initial Commit

---
+++ FaceLockProject/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FaceLockProject
+//
+//  Created by 姚中天 on 2020/1/30.
+//  Copyright © 2020 zh0ngtian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
