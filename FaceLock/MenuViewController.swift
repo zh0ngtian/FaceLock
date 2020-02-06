343ac43a09cb7c1924b18ba6b6c6fe3142fdd588
YaoZhontian <305301890@qq.com>
2020-02-07 02:03:25 +0800

    fix bugs: type password after unlocking

--- FaceLock/MenuViewController.swift
+++ FaceLock/MenuViewController.swift
@@ -19,8 +19,7 @@ class MenuViewController: NSObject, NSApplicationDelegate, NSUserNotificationCen
     var KEY_FOR_FEA = "user_face_feature"
     var KEY_FOR_PASSWD = "user_password"
     
-    // MARK: - 工具函数
-    // 请求辅助功能权限
+    // MARK: - Utils
     func askForAccessibility() {
         let checkOptPrompt = kAXTrustedCheckOptionPrompt.takeRetainedValue() as String
         let accessEnabled = AXIsProcessTrustedWithOptions([checkOptPrompt: true] as CFDictionary)
@@ -33,7 +32,6 @@ class MenuViewController: NSObject, NSApplicationDelegate, NSUserNotificationCen
         }
     }
     
-    // 完成拍照或者照片选择后的回调
     @objc func pictureTakerValidated(pictureTaker: IKPictureTaker, returnCode: NSInteger, contextInfo: UnsafeRawPointer) {
         if (returnCode == NSApplication.ModalResponse.OK.rawValue) {
             let outputImage: NSImage = pictureTaker.outputImage()
@@ -43,7 +41,6 @@ class MenuViewController: NSObject, NSApplicationDelegate, NSUserNotificationCen
         }
     }
     
-    // 模拟键盘输入
     func fakeKeyStrokes(str: String) {
         let src = CGEventSource(stateID: .hidSystemState)
         let pressEvent = CGEvent(keyboardEventSource: src, virtualKey: 49, keyDown: true)
@@ -59,7 +56,6 @@ class MenuViewController: NSObject, NSApplicationDelegate, NSUserNotificationCen
         CGEvent(keyboardEventSource: src, virtualKey: 52, keyDown: false)?.post(tap: .cghidEventTap)
     }
     
-    // 请求用户密码
     func askForPassword() -> String {
         func t(_ key: String) -> String {
             return NSLocalizedString(key, comment: "")
@@ -84,8 +80,7 @@ class MenuViewController: NSObject, NSApplicationDelegate, NSUserNotificationCen
         return ""
     }
     
-    // MARK: - 解锁屏幕
-    // 判断当前是否锁屏
+    // MARK: - Unlock
     func isScreenLocked() -> Bool {
         if let dict = CGSessionCopyCurrentDictionary() as? [String : Any] {
             if let locked = dict["CGSSessionScreenIsLocked"] as? Int {
@@ -95,54 +90,66 @@ class MenuViewController: NSObject, NSApplicationDelegate, NSUserNotificationCen
         return false
     }
     
-    // 尝试解锁屏幕
     func tryUnlockScreen() {
-        guard self.isScreenLocked() else { return }
+        print("\(Date(timeIntervalSinceNow: 0)) -> Try to unlock screen.")
+        guard !self.displaySleep && self.isScreenLocked() else { return }
         
-        // 检查相机权限
+        // check cemera
         let status = AVCaptureDevice.authorizationStatus(for: .video)
-        guard status == .authorized else { return }
+        guard status == .authorized else {
+            print("\(Date(timeIntervalSinceNow: 0)) -> No cemera authority.")
+            return
+        }
         
-        // 检查辅助功能权限
+        // check accessibility
         let checkOptPrompt = kAXTrustedCheckOptionPrompt.takeRetainedValue() as String
         let accessEnabled = AXIsProcessTrustedWithOptions([checkOptPrompt: true] as CFDictionary)
-        guard accessEnabled else { return }
+        guard accessEnabled else {
+            print("\(Date(timeIntervalSinceNow: 0)) -> No accessibility authority.")
+            return
+        }
         
-        // 检查是否设置人脸
+        // check face
         let feaArray = UserDefaults.standard.array(forKey: KEY_FOR_FEA)
-        guard feaArray != nil else { return }
+        guard feaArray != nil else {
+            print("\(Date(timeIntervalSinceNow: 0)) -> No face saved.")
+            return
+        }
+        let feaMutatle = NSMutableArray()
+        feaMutatle.addObjects(from: feaArray!)
         
-        // 检查是否设置密码
+        // check password
         let passwd = UserDefaults.standard.string(forKey: KEY_FOR_PASSWD)
-        guard passwd != nil else { return }
+        guard passwd != nil else {
+            print("\(Date(timeIntervalSinceNow: 0)) -> No password saved.")
+            return
+        }
         
         let pc = PhotoCapturer()
         pc.startCapture()
         let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
-            let feaMutatle = NSMutableArray()
-            feaMutatle.addObjects(from: feaArray!)
-                        
             let similarity = self.recognizer.verify(pc.capturedImage, withTargetFea: feaMutatle)
-            if similarity > 0.65 {
-                self.fakeKeyStrokes(str: passwd!)
+            print("similarity: ", similarity)
+            
+            if similarity > 0.8 {
+                if !self.displaySleep && self.isScreenLocked() {
+                    self.fakeKeyStrokes(str: passwd!)
+                }
                 pc.stopCapture()
                 timer.invalidate()
-            } else {
-                print("Wrong face.")
             }
             
-            if self.displaySleep {
-                timer.invalidate()
+            if self.displaySleep || !self.isScreenLocked() {
                 pc.stopCapture()
+                timer.invalidate()
             }
         })
         RunLoop.current.add(timer, forMode: .common)
     }
     
-    // MARK: - 状态处理
-    // 解锁时
+    // MARK: - Process
     @objc func onUnlock() {
-        print("\(Date(timeIntervalSinceNow: 0)) -> onUnlock")
+        print("\(Date(timeIntervalSinceNow: 0)) -> on unlock")
         
         if !self.keyMouseMonitors.isEmpty {
             let keyMouseMonitor = keyMouseMonitors[0]
@@ -153,9 +160,8 @@ class MenuViewController: NSObject, NSApplicationDelegate, NSUserNotificationCen
         self.screenSaverDidStart = false
     }
     
-    // 显示器睡眠时
     @objc func onDisplaySleep() {
-        print("\(Date(timeIntervalSinceNow: 0)) -> onDisplaySleep")
+        print("\(Date(timeIntervalSinceNow: 0)) -> on display sleep")
         
         if !self.keyMouseMonitors.isEmpty {
             let keyMouseMonitor = keyMouseMonitors[0]
@@ -166,16 +172,14 @@ class MenuViewController: NSObject, NSApplicationDelegate, NSUserNotificationCen
         self.screenSaverDidStart = false
     }
     
-    // 显示器唤醒时
     @objc func onDisplayWake() {
-        print("\(Date(timeIntervalSinceNow: 0)) -> onDisplayWake")
+        print("\(Date(timeIntervalSinceNow: 0)) -> on display wake")
         self.displaySleep = false
         self.tryUnlockScreen()
     }
     
-    // 屏保开始时
     @objc func onScreensaverDidStart() {
-        print("\(Date(timeIntervalSinceNow: 0)) -> onScreensaverDidStart")
+        print("\(Date(timeIntervalSinceNow: 0)) -> on screensaver didStart")
         
         self.screenSaverDidStart = true
         let keyMouseMonitor = NSEvent.addGlobalMonitorForEvents(matching: [.keyDown, .mouseMoved]) { _ in
@@ -184,8 +188,7 @@ class MenuViewController: NSObject, NSApplicationDelegate, NSUserNotificationCen
         keyMouseMonitors.append(keyMouseMonitor)
     }
     
-    // MARK: - 初始化
-    // 类初始化
+    // MARK: - Init
     override init() {
         let fullPath = String(Bundle.main.path(forResource: "det1", ofType: "bin")!)
         let deRange = fullPath.range(of: "/det1.bin")
@@ -194,7 +197,6 @@ class MenuViewController: NSObject, NSApplicationDelegate, NSUserNotificationCen
         super.init()
     }
     
-    // 菜单初始化
     override func awakeFromNib() {
         if let button = faceStatusItem.button {
             button.image = NSImage(named: "StatusIcon")
@@ -211,35 +213,46 @@ class MenuViewController: NSObject, NSApplicationDelegate, NSUserNotificationCen
         dnc.addObserver(self, selector: #selector(onScreensaverDidStart), name: NSNotification.Name(rawValue: "com.apple.screensaver.didstart"), object: nil)
     }
     
-    // MARK: - 按钮操作
-     @IBOutlet weak var faceMenu: NSMenu!
-     let faceStatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    // MARK: - Buttons
+    @IBOutlet weak var faceMenu: NSMenu!
+    let faceStatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
     
-    // Add Face
     @IBAction func addFaceAction(_ sender: NSMenuItem) {
         let pictureTaker = IKPictureTaker()
-        let picUrl: URL
-        let bundle = Bundle()
-        if (bundle.isLoaded) {
-            let picPath = bundle.path(forResource: "picture", ofType: "jpg")
+        pictureTaker.setValue(false, forKey: IKPictureTakerAllowsEditingKey)
+        if (Bundle().isLoaded) {
+            let picPath = Bundle().path(forResource: "picture", ofType: "jpg")
             if ((picPath) != nil) {
-                picUrl = URL.init(fileURLWithPath: picPath!)
+                let picUrl = URL.init(fileURLWithPath: picPath!)
                 pictureTaker.setInputImage(NSImage.init(byReferencing: picUrl))
             }
         }
         pictureTaker.begin(withDelegate: self, didEnd: #selector(pictureTakerValidated(pictureTaker:returnCode:contextInfo:)), contextInfo: nil)
     }
     
-    // Add Password
     @IBAction func addPasswordAction(_ sender: NSMenuItem) {
-        // TODO: 添加大图标和小图标
         let passwd = self.askForPassword()
         if passwd != "" {
             UserDefaults.standard.set(passwd, forKey: KEY_FOR_PASSWD)
         }
     }
     
-    // Quit
+    @IBAction func debugAction(_ sender: NSMenuItem) {
+        let feaArray = UserDefaults.standard.array(forKey: KEY_FOR_FEA)
+        guard feaArray != nil else { return }
+        let feaMutatle = NSMutableArray()
+        feaMutatle.addObjects(from: feaArray!)
+        
+        let pc = PhotoCapturer()
+        pc.startCapture()
+        let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
+            let ci = pc.capturedImage
+            let similarity = self.recognizer.verify(ci, withTargetFea: feaMutatle)
+            print("similarity: ", similarity)
+        })
+        RunLoop.current.add(timer, forMode: .common)
+    }
+    
     @IBAction func quitAction(_ sender: NSMenuItem) {
         NSApplication.shared.terminate(self)
     }
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # FaceLock
-Unlock your mac with your face.
+**FaceLock** is a small menu bar application that can unlock your mac with your face.
+
+## Features
+
+## Requirements
+
+macOS 10.14 (Mojave) or later
+
+## Installation
+
+## Attention
+
+FaceLock does not yet support face anti-spoofing, which means that your photos can be used to unlock your mac.
+
+## TODO
+
+- [ ] Support more faces
+- [ ] Use more powerful CNN model
+- [ ] Support simple face anti-spoofing
+
+## Reference
+
+* [BLEUnlock](https://github.com/ts1/BLEUnlock)
+* [ncnn-mtcnn-facenet](https://github.com/xuduo35/ncnn-mtcnn-facenet)
+
+* Icon made by [smashicons](https://www.flaticon.com/authors/smashicons) from [www.flaticon.com](http://www.flaticon.com/)
+
